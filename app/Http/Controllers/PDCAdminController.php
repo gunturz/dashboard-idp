3439983b743511cfa24e8180a627d8cf8ed9c68c
Hamid Izzudin Al Fitroh <hamidizzudinalfitroh04@gmail.com>
2026-04-07 10:03:20 +0700

    export

--- app/Http/Controllers/PDCAdminController.php
+++ app/Http/Controllers/PDCAdminController.php
@@ -14,6 +14,7 @@ use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class PDCAdminController extends Controller
 {

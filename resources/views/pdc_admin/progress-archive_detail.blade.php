e46f7c3d07ebf2808101132ad892b1a34a88233d
gunturz <prasetyoguntur045@gmail.com>
2026-05-25 14:53:48 +0700

    UI: Remove LogBook title and empty state from progress archive detail view

--- resources/views/pdc_admin/progress-archive_detail.blade.php
+++ resources/views/pdc_admin/progress-archive_detail.blade.php
@@ -1127,20 +1127,7 @@
             </div>
         </div>
 
-        <hr class="border-t-2 border-slate-200 my-8">
 
-        <div class="sub-section-title">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-            </svg>
-            LogBook
-        </div>
-
-        <div id="archive-logbook-empty"
-            class="bg-slate-50 border border-dashed border-slate-300 rounded-xl px-6 py-8 text-center text-sm font-semibold text-slate-500">
-            Pilih Exposure, Mentoring, atau Learning untuk melihat logbook.
-        </div>
 
         {{-- Exposure Logbook --}}
         @php $exposureActivities = $talent->idpActivities->filter(fn($a) => str_contains(strtolower(optional($a->type)->type_name ?? ''), 'exposure')); @endphp

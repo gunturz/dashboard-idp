aea70c3b298a834b06197618103e7e9d2ce8095a
gunturz <prasetyoguntur045@gmail.com>
2026-05-29 09:12:15 +0700

    ubah warna tombol edit di admin

--- resources/views/livewire/pdc-company-management.blade.php
+++ resources/views/livewire/pdc-company-management.blade.php
@@ -64,8 +64,8 @@
                         {{-- Edit --}}
                         <button type="button"
                             wire:click="openEditModal({{ $company->id }}, '{{ addslashes($company->nama_company) }}')"
-                            class="flex items-center justify-center w-10 h-10 bg-[#F4F1EA] hover:bg-[#eadecc] border border-[#e5e1d8] rounded-xl transition-colors">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-[#475569]" fill="none"
+                            class="flex items-center justify-center w-10 h-10 bg-blue-500 hover:bg-blue-600 border border-blue-600 hover:border-blue-700 rounded-xl transition-colors shadow-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" fill="none"
                                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />

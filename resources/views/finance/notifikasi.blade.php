8750e017e3dc3dd298d961c80fd262354cc3c727
gunturz <prasetyoguntur045@gmail.com>
2026-06-12 14:59:49 +0700

    responsive profil

--- resources/views/finance/notifikasi.blade.php
+++ resources/views/finance/notifikasi.blade.php
@@ -45,9 +45,11 @@
         </style>
     </x-slot>
 
-    <div class="px-6 pb-8">
-        <div class="mx-auto w-full animate-title" style="max-width: 960px;">
-            <livewire:finance-notifikasi-list />
+    <div class="-mx-4 lg:-mx-6">
+        <div class="mx-auto w-full pb-8 px-4 lg:px-6">
+            <div class="mx-auto w-full animate-title" style="max-width: 960px;">
+                <livewire:finance-notifikasi-list />
+            </div>
         </div>
     </div>
 
